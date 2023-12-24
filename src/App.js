f13decabeae42ec1b1fd59a87d6c5c85b4d444dc
Lidor Sanker <57188775+lidor7776@users.noreply.github.com>
2023-12-24 17:51:30 +0200

    Add redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
-
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "./features/userSlice";
 function App() {
+
+  const {user}=useSelector((state)=>state.user)
+  console.log(user)
   return (
     <div className="dark">
       
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/register" element={<Register/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
