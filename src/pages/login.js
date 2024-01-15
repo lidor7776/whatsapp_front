aa02accb4fef76ec1735d38a742407b333a640b6
Lidor Sanker <57188775+lidor7776@users.noreply.github.com>
2024-01-15 14:48:44 +0200

    Add tailwind to login/register

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import LoginForm from '../components/auth/LoginForm'
 
 function Login() {
   return (
-    <div>L</div>
+    <div className="h-screen dark:bg-dark_bg_1 felx items-center justify-center py-[19px] overflow-hidden">
+      {/*Container */}
+      <div className="flex w-[1600px] mx-auto h-full">
+        <LoginForm/>
+      </div>
+    </div>
   )
 }
 
